4334711824425d09be53eb6c4da65e9250d4b61d
nulltu <rusbent.matta@gmail.com>
2020-08-31 20:49:14 -0300

    test bund

---
+++ backend/models/UserModel.js
@@ -0,0 +1,15 @@
+// const mongoose = require('mongoose')
+
+// const userSchema = new mongoose.Schema({
+
+//     urlPhoto : {type:String, required:true},
+//     username :{type:String, required:true},
+//     password: {type:String, required:true},
+//     email:{type:string, required:true},
+//     firsName:{type:string, required:true},
+//     lastName:{type:string, required:true}
+   
+// })
+
+
+
